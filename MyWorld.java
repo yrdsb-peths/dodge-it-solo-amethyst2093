6d1b9c73f5557ebd9b4588b3fb45ca7e0b866608
amethyst2093 <wonton0409@gmail.com>
2025-04-14 09:01:47 -0400

    reset banana

--- MyWorld.java
+++ MyWorld.java
@@ -5,5 +5,7 @@ public class MyWorld extends World {
         super(600, 400, 1);
         Teddy teddy = new Teddy();
         addObject(teddy, 100, 100);
+        Banana banana = new Banana();
+        addObject(banana, 600, 100);
     }
 }
