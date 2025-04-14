fd8944f066935f757964deb8de40311f77aba6d9
amethyst2093 <wonton0409@gmail.com>
2025-04-14 08:46:18 -0400

    added teddy character

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Teddy teddy = new Teddy();
+        addObject(teddy, 100, 100);
     }
 }
