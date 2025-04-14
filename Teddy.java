0cd441de75bf66c3cb1953e7b39929a9c92a96ae
amethyst2093 <wonton0409@gmail.com>
2025-04-14 08:54:46 -0400

    add banana + move teddy

--- Teddy.java
+++ Teddy.java
@@ -1,19 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Teddy here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Teddy extends Actor
 {
-    /**
-     * Act - do whatever the Teddy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    boolean atTop = true;
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(null)){
+            atTop = !atTop;
+        }
+        if(atTop){
+            setLocation(100,100);
+        } else {
+            setLocation(100,300);
+        }
     }
 }
