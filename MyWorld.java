af45c97265c20e3012705ef4a922311195df512e
amethyst2093 <wonton0409@gmail.com>
2025-04-15 08:49:26 -0400

    teddy starts in middle, banana is slower

--- MyWorld.java
+++ MyWorld.java
@@ -4,7 +4,7 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 600, 1);
         Teddy teddy = new Teddy();
-        addObject(teddy, 100, 100);
+        addObject(teddy, 100, 300);
         Banana banana = new Banana();
         addObject(banana, 600, 100);
     }
